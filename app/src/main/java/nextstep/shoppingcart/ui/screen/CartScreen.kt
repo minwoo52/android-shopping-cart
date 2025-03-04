20d402a41d1cdbaf91f438f01c7a668fd1dfd760
minoo <minwoo@dailyshot.co>
2025-03-04 23:56:39 +0900

    feat: 장바구니 페이지 화면 추가

---
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/CartScreen.kt
@@ -0,0 +1,51 @@
+package nextstep.shoppingcart.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.R
+import nextstep.shoppingcart.ui.component.BackButtonAppBar
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+import nextstep.shoppingcart.ui.theme.White
+
+@Composable
+fun CartScreen(
+    onBackButtonClick: () -> Unit
+) {
+    val appBarTitle = stringResource(R.string.title_cart)
+
+    Scaffold(
+        topBar = {
+            BackButtonAppBar(
+                title = appBarTitle,
+                onBackClick = { onBackButtonClick() },
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(White)
+                .padding(innerPadding)
+        ) {
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CartScreenPreview() {
+    ShoppingCartTheme {
+        CartScreen(
+            onBackButtonClick = {
+                // NO-OP
+            }
+        )
+    }
+}
