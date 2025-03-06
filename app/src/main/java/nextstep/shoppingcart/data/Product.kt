98cd34f0343fa123bad003fedc53b9270c7669cb
minoo <minwoo@dailyshot.co>
2025-03-06 22:47:33 +0900

    refactor: Product 데이터를 Parcelable로 변경하여 직렬화 지원

--- app/src/main/java/nextstep/shoppingcart/data/Product.kt
+++ app/src/main/java/nextstep/shoppingcart/data/Product.kt
@@ -1,15 +1,15 @@
 package nextstep.shoppingcart.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.Locale
 
+@Parcelize
 data class Product(
     val imageUrl: String,
     val name: String,
     val price: Int,
-) {
+) : Parcelable {
     val formattedPrice: String
         get() = String.format(Locale.KOREA, "%,d원", price)
-
-    val randomId: String
-        get() = (0..1000).random().toString()
 }
